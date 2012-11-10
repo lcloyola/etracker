7545b6a204e77c8b6356ed5719dc99fe4e18f7ea
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-11-10 17:11:00 +0800

    layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Etracker::Application.routes.draw do
   devise_for :users
+  root :to => 'home#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -58,3 +59,4 @@ Etracker::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
