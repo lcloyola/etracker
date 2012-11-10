0040454d404edd4382e6d277e2ff44a0e9c3d4b1
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2012-11-10 15:12:06 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etracker::Application.config.secret_token = 'e9fbb84e584f85a77c52c0e64d4a42179187a1ff1dda2cc9767a252ee0a4a8997602713f7813d79d4aafa775a79e89196f519634824dc2d57cc3fbb4197bc31a'
