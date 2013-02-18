1dcd71e6233dd1af19547ed0a8e58d891d2bc9e2
Lizbeth Loyola <lizbeth@crunchbang.(none)>
2013-02-18 15:08:21 +0800

    removed status_id in logs

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,6 @@
 class Log < ActiveRecord::Base
   belongs_to :unit
   belongs_to :user
-  attr_accessible :remarks, :status, :unit_id, :status_id, :user_id
+  attr_accessible :remarks, :status, :unit_id, :user_id
 end
 
